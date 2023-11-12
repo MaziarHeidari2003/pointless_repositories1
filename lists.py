b913e0c087fc211e8e2df3cbc66af475d27dc4b3
MaziarHeidari2003 <maziar1124@gmail.com>
2023-11-12 13:56:51 -0800

    pointless_commit.py

---
+++ lists.py
@@ -0,0 +1,8 @@
+food = ["pizza","hambergur","hotdog","spaghtti"]
+drink = ["soda","water","tea"]
+food.append("ice cream")
+food.remove("hotdog")
+food.insert(0,"cake")
+
+basicNeeds = [food,drink]
+print(basicNeeds[1][2])
