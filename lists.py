7e6d8bdb461a3790af5311521a27253c0a05df8c
MaziarHeidari2003 <maziar1124@gmail.com>
2023-11-15 12:45:47 -0800

    pointless_commits 10

--- lists.py
+++ lists.py
@@ -1,8 +1,13 @@
-food = ["pizza","hambergur","hotdog","spaghtti"]
-drink = ["soda","water","tea"]
-food.append("ice cream")
-food.remove("hotdog")
-food.insert(0,"cake")
+import os
 
-basicNeeds = [food,drink]
-print(basicNeeds[1][2])
+path = "D:\\Desktop\\all_python_practices\\bitch.txt"
+
+if os.path.exists(path):
+    print("that location exists")
+    if os.path.isfile:
+        print("that is a file")
+    elif os.path.isdir:
+        print("its a directory")    
+
+else:
+    print("nothing!!!")        
