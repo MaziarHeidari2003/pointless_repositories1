6b2a0ff5429161cecaa57523f5342f069023fad8
MaziarHeidari2003 <maziar1124@gmail.com>
2023-11-15 23:28:11 -0800

    radioCheck2

--- filespracticing.py
+++ filespracticing.py
@@ -1,10 +1,48 @@
 #try:
-   # with open('bitch.txt') as file:
-    #   print (file.read())
+   # with open('text.txt') as file:
+    #    print(file.read())
+
 #except FileNotFoundError:
  #   print("that file was not found")
-#print(file.closed)    
 
-text = "yoooo\n this is some new text\n"
-with open('text.txt','w') as file:
-    file.write(text)
+#text = "yoo\n this is some text \n so how are you\n"
+#with open('practice.txt', 'w') as file:
+   # file.write(text)
+
+#import shutil 
+#shutil.copyfile('text.txt','copy.txt')
+
+#import os
+
+#source = 'copy.txt'
+#destination = 'D:\\Desktop\\replacement.txt'
+ #try:
+
+    #if os.path.exists(destination):
+     #   print("there is already a file in there")
+   # else:
+       # os.replace(source,destination)
+      #  print(source+ "was moved")    
+ 
+
+ #except FileNotFoundError:
+  #      print(source+" was not found")
+
+
+
+import os
+import shutil
+
+path = 'folder1'
+try:  
+   # os.rmdir(path)
+   shutil.rmtree(path)
+
+except FileNotFoundError:
+    print("that file wasnt found") 
+except PermissionError:
+    print('no bitch, no!!!')
+except OSError:
+    print('you are using the wrong function asshole')    
+else:
+    print('path was deleted')      
